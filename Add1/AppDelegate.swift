842b946d9f06d62364d33a1c37b483fbaa875246
iPpanther <tang.peerapol@gmail.com>
2019-03-16 16:49:41 +0530

    develop part 1

--- Add1/AppDelegate.swift
+++ Add1/AppDelegate.swift
@@ -13,9 +13,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    var mainVC: MainViewController?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        mainVC = MainViewController(nibName: "MainViewController", bundle:nil)
+        
+        let frame = UIScreen.main.bounds
+        window = UIWindow(frame: frame)
+        
+        window!.rootViewController = mainVC
+        window!.makeKeyAndVisible()
+        
         return true
     }
 
---
+++ Add1/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  Add1
+//
+//  Created by Peerapol on 16/3/2562 BE.
+//  Copyright © 2562 iPpanther. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
